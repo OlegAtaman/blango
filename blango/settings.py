019be629ad3000756ad26e00254a1c27e756e58c
Oleg Ataman <o.ataman.tv11@kpi.ua>
2023-04-16 17:19:31 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -77,6 +77,7 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         'rest_framework',
+        'rest_framework.authtoken',
     ]
 
     AUTH_USER_MODEL = "blango_auth.User"
@@ -93,6 +94,14 @@ class Dev(Configuration):
     ]
 
     ROOT_URLCONF = 'blango.urls'
+
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
     TEMPLATES = [
         {
